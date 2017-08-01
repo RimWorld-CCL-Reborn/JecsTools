94a837395ff4a49643f2266ab8fb53cfac12f83d
jecrell <matt.walls31@gmail.com>
2017-08-01 12:03:34 +0900

    v1.0.5
    v1.0.5
    8/1/17
    
    -Fixed faction issue when entering/exiting vehicles.
    -Added new primary assembly, JecsTools with several new classes.
    -New Classes
    
    JecsTools.Hediff_TransformedPart
    - Similar to added part, however, transformed parts will not remove the
    original parts when removed from the character. This allows for us to
    "transform" pawn parts. Such as having a colonists' hands turn into
    deadly claws.
    ((Will see use in upcoming Werewolf mod))
    
    JecsTools.JobGiver_AIFirelessTrashColonyClose
    JecsTools.JobGiver_AIFirelessTrashColonyDistant
    - These classes lets us call a special jobgiver for raiders that does
    not include setting fire to objects. This is good for monstrous
    creatures that do not have the ability to start fires but still want to
    break things.
    ((Will see use in Werewolf mod and Cosmic Horrors))

---
+++ Source/AllModdingComponents/JecsTools/FirelessTrashUtility.cs
@@ -0,0 +1,102 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+namespace JecsTools
+{
+    public static class FirelessTrashUtility
+    {
+        // RimWorld.TrashUtility
+        public static Job TrashJob(Pawn pawn, Thing t)
+        {
+            Plant plant = t as Plant;
+            if (plant != null)
+            {
+                Job job = new Job(JobDefOf.AttackMelee, t);
+                FinalizeTrashJob(job);
+                return job;
+            }
+            if (pawn.equipment != null && Rand.Value < 0.7f)
+            {
+                foreach (Verb current in pawn.equipment.AllEquipmentVerbs)
+                {
+                    if (current.verbProps.ai_IsBuildingDestroyer)
+                    {
+                        Job job2 = new Job(JobDefOf.UseVerbOnThing, t);
+                        job2.verbToUse = current;
+                        FinalizeTrashJob(job2);
+                        return job2;
+                    }
+                }
+            }
+            float value = Rand.Value;
+            Job job3 = new Job(JobDefOf.AttackMelee, t);
+            FinalizeTrashJob(job3);
+            return job3;
+        }
+
+        // RimWorld.TrashUtility
+        private static readonly IntRange TrashJobCheckOverrideInterval = new IntRange(450, 500);
+
+        // RimWorld.TrashUtility
+        private static void FinalizeTrashJob(Job job)
+        {
+            job.expiryInterval = TrashJobCheckOverrideInterval.RandomInRange;
+            job.checkOverrideOnExpire = true;
+            job.expireRequiresEnemiesNearby = true;
+        }
+
+
+        // RimWorld.TrashUtility
+        public static bool ShouldTrashBuilding(Pawn pawn, Building b)
+        {
+            if (!b.def.useHitPoints)
+            {
+                return false;
+            }
+            if (b.def.building.isInert || b.def.building.isTrap)
+            {
+                int num = GenLocalDate.HourOfDay(pawn) / 3;
+                int specialSeed = b.GetHashCode() * 612361 ^ pawn.GetHashCode() * 391 ^ num * 734273247;
+                if (!Rand.ChanceSeeded(0.008f, specialSeed))
+                {
+                    return false;
+                }
+            }
+            return (!b.def.building.isTrap || !((Building_Trap)b).Armed) && CanTrash(pawn, b) && pawn.HostileTo(b);
+        }
+
+
+
+        // RimWorld.TrashUtility
+        public static bool ShouldTrashPlant(Pawn pawn, Plant p)
+        {
+            if (!p.sown || p.def.plant.IsTree || !p.FlammableNow || !CanTrash(pawn, p))
+            {
+                return false;
+            }
+            CellRect.CellRectIterator iterator = CellRect.CenteredOn(p.Position, 2).ClipInsideMap(p.Map).GetIterator();
+            while (!iterator.Done())
+            {
+                IntVec3 current = iterator.Current;
+                if (current.InBounds(p.Map) && current.ContainsStaticFire(p.Map))
+                {
+                    return false;
+                }
+                iterator.MoveNext();
+            }
+            return p.Position.Roofed(p.Map) || p.Map.weatherManager.RainRate <= 0.25f;
+        }
+
+        // RimWorld.TrashUtility
+        private static bool CanTrash(Pawn pawn, Thing t)
+        {
+            return pawn.CanReach(t, PathEndMode.Touch, Danger.Some, false, TraverseMode.ByPawn) && !t.IsBurning();
+        }
+
+    }
+}
---
+++ Source/AllModdingComponents/JecsTools/JobGiver_AIFirelessTrashColonyClose.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace JecsTools
+{
+    public class JobGiver_AIFirelessTrashColonyClose : ThinkNode_JobGiver
+    {
+        private const int CloseSearchRadius = 5;
+
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            if (!pawn.HostileTo(Faction.OfPlayer))
+            {
+                return null;
+            }
+            bool flag = pawn.natives.IgniteVerb != null && pawn.HostileTo(Faction.OfPlayer);
+            CellRect cellRect = CellRect.CenteredOn(pawn.Position, 5);
+            for (int i = 0; i < 35; i++)
+            {
+                IntVec3 randomCell = cellRect.RandomCell;
+                if (randomCell.InBounds(pawn.Map))
+                {
+                    Building edifice = randomCell.GetEdifice(pawn.Map);
+                    if (edifice != null && FirelessTrashUtility.ShouldTrashBuilding(pawn, edifice) && GenSight.LineOfSight(pawn.Position, randomCell, pawn.Map, false, null, 0, 0))
+                    {
+                        if (DebugViewSettings.drawDestSearch && Find.VisibleMap == pawn.Map)
+                        {
+                            Find.VisibleMap.debugDrawer.FlashCell(randomCell, 1f, "trash bld");
+                        }
+                        return FirelessTrashUtility.TrashJob(pawn, edifice);
+                    }
+                    if (flag)
+                    {
+                        Plant plant = randomCell.GetPlant(pawn.Map);
+                        if (plant != null && FirelessTrashUtility.ShouldTrashPlant(pawn, plant) && GenSight.LineOfSight(pawn.Position, randomCell, pawn.Map, false, null, 0, 0))
+                        {
+                            if (DebugViewSettings.drawDestSearch && Find.VisibleMap == pawn.Map)
+                            {
+                                Find.VisibleMap.debugDrawer.FlashCell(randomCell, 0.5f, "trash plant");
+                            }
+                            return FirelessTrashUtility.TrashJob(pawn, plant);
+                        }
+                    }
+                    if (DebugViewSettings.drawDestSearch && Find.VisibleMap == pawn.Map)
+                    {
+                        Find.VisibleMap.debugDrawer.FlashCell(randomCell, 0f, "trash no");
+                    }
+                }
+            }
+            return null;
+        }
+
+
+
+    }
+}
