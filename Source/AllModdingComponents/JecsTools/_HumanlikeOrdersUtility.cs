135ecd7e737b6a076561c9a8987951fe785e686c
jecrell <matt.walls31@gmail.com>
2017-11-25 21:39:27 +0900

    Quick update

--- Source/AllModdingComponents/JecsTools/_HumanlikeOrdersUtility.cs
+++ Source/AllModdingComponents/JecsTools/_HumanlikeOrdersUtility.cs
@@ -39,41 +39,47 @@ namespace JecsTools
             {
                 if (floatMenuOptionList == null)
                 {
-                    //Log.Message("FMOL :: Initialized List");
+                    DebugMessage("FMOL :: Initialized List");
                     floatMenuOptionList = new Dictionary<_Condition, List<Func<Vector3, Pawn, Thing, List<FloatMenuOption>>>>();
                 }
                 return floatMenuOptionList;
             }
         }
 
+        private static bool debugMode = true;
+        public static void DebugMessage(string s)
+        {
+            if (debugMode) Log.Message(s);
+        }
+
         static _HumanlikeOrdersUtility()
         {
-            //Log.Message("FMOL :: Initialized Constructor");
-            floatMenuID = 0;
-            lastSavedFloatMenuID = -1;
+            DebugMessage("FMOL :: Initialized Constructor");
+            //floatMenuID = 0;
+            //lastSavedFloatMenuID = -1;
             foreach (Type current in typeof(FloatMenuPatch).AllSubclassesNonAbstract())
             {
 
                 FloatMenuPatch item = (FloatMenuPatch)Activator.CreateInstance(current);
 
-                //Log.Message("FMOL :: Enter Loop Step");
+                DebugMessage("FMOL :: Enter Loop Step");
                 IEnumerable<KeyValuePair<_Condition, Func<Vector3, Pawn, Thing, List<FloatMenuOption>>>> floatMenus = item.GetFloatMenus();
-                //Log.Message("FMOL :: Float Menus Variable Declared");
+                DebugMessage("FMOL :: Float Menus Variable Declared");
 
                 if (floatMenus != null && floatMenus.Count() > 0)
                 {
-                    //Log.Message("FMOL :: Float Menus Available Check Passed");
+                    DebugMessage("FMOL :: Float Menus Available Check Passed");
                     foreach (KeyValuePair<_Condition, Func<Vector3, Pawn, Thing, List<FloatMenuOption>>> floatMenu in floatMenus)
                     {
-                        //Log.Message("FMOL :: Enter Float Menu Check Loop");
+                        DebugMessage("FMOL :: Enter Float Menu Check Loop");
                         if (FloatMenuOptionList.ContainsKey(floatMenu.Key))
                         {
-                            //Log.Message("FMOL :: Existing condition found for " + floatMenu.Key.ToString() + " adding actions to dictionary.");
+                            DebugMessage("FMOL :: Existing condition found for " + floatMenu.Key.ToString() + " adding actions to dictionary.");
                             FloatMenuOptionList[floatMenu.Key].Add(floatMenu.Value);
                         }
                         else
                         {
-                            //Log.Message("FMOL :: Existing condition not found for " + floatMenu.Key.ToString() + " adding key and actions to dictionary.");
+                            DebugMessage("FMOL :: Existing condition not found for " + floatMenu.Key.ToString() + " adding key and actions to dictionary.");
                             FloatMenuOptionList.Add(floatMenu.Key, new List<Func<Vector3, Pawn, Thing, List<FloatMenuOption>>>() { floatMenu.Value });
                         }
                     }
@@ -82,34 +88,43 @@ namespace JecsTools
 
             HarmonyInstance harmony = HarmonyInstance.Create("rimworld.jecrell.humanlikeorders");
             harmony.Patch(AccessTools.Method(typeof(FloatMenuMakerMap), "AddHumanlikeOrders"), null, new HarmonyMethod(typeof(_HumanlikeOrdersUtility), nameof(AddHumanlikeOrders_PostFix)));
-            harmony.Patch(AccessTools.Method(typeof(FloatMenuMakerMap), "CanTakeOrder"), null, new HarmonyMethod(typeof(_HumanlikeOrdersUtility), nameof(CanTakeOrder_PostFix)));
+            //harmony.Patch(AccessTools.Method(typeof(FloatMenuMakerMap), "CanTakeOrder"), null, new HarmonyMethod(typeof(_HumanlikeOrdersUtility), nameof(CanTakeOrder_PostFix)));
         }
 
-        private static int floatMenuID = 0;
-        private static int lastSavedFloatMenuID = -1;
+        //private static int floatMenuID = 0;
+        //private static int lastSavedFloatMenuID = -1;
         private static List<FloatMenuOption> savedList = new List<FloatMenuOption>();
+        public static string optsID = "";
+        public static string lastOptsID = "1";
 
-        private static void CanTakeOrder_PostFix(Pawn pawn, ref bool __result)
-        {
-            if (__result)
-            {
-                floatMenuID = Rand.Range(1000, 9999);
-                //Log.Message("FMOL :: ID Set to " + floatMenuID);
-            }
-        }
+
+        //private static void CanTakeOrder_PostFix(Pawn pawn, ref bool __result)
+        //{
+        //    if (__result)
+        //    {
+        //        floatMenuID = Rand.Range(1000, 9999);
+        //        DebugMessage("FMOL :: ID Set to " + floatMenuID);
+        //    }
+        //}
+
 
         // RimWorld.FloatMenuMakerMap
         public static void AddHumanlikeOrders_PostFix(Vector3 clickPos, Pawn pawn, List<FloatMenuOption> opts)
         {
             IntVec3 c = IntVec3.FromVector3(clickPos);
-            //Log.Message("FMOL :: Check ID: " + floatMenuID + " vs SaveID: " +lastSavedFloatMenuID);
-            if (lastSavedFloatMenuID == floatMenuID)
+            optsID = "";
+            optsID += pawn.ThingID;
+            for (int i = 0; i < (opts?.Count() ?? 0); i++)
             {
-                //Log.Message("FMOL :: ID Set to " + floatMenuID);
+                optsID += opts[i].Label;
+            }
+            if (optsID == lastOptsID)
+            {
                 opts.AddRange(savedList);
                 return;
             }
-            lastSavedFloatMenuID = floatMenuID;
+            DebugMessage("FMOL :: New list constructed");
+            lastOptsID = optsID;
             savedList.Clear();
             if (c.GetThingList(pawn.Map) is List<Thing> things && !things.NullOrEmpty())
             {
@@ -171,7 +186,6 @@ namespace JecsTools
 
         public bool Passes(object toCheck)
         {
-            Log.Message("Passes Called");
             switch (Condition)
             {
                 case _ConditionType.IsType:
