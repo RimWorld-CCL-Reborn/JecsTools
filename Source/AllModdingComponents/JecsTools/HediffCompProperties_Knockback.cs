c24fc8c2228191dac27f5c06f31296759aff5695
jecrell <matt.walls31@gmail.com>
2018-07-01 13:02:47 +0900

    Adjustments made

--- Source/AllModdingComponents/JecsTools/HediffCompProperties_Knockback.cs
+++ Source/AllModdingComponents/JecsTools/HediffCompProperties_Knockback.cs
@@ -5,11 +5,11 @@ namespace JecsTools
 {
     public class HediffCompProperties_Knockback : HediffCompProperties
     {
-        public DamageDef explosionDmg = DamageDefOf.Stun;
+        public DamageDef explosionDmg = DefDatabase<DamageDef>.GetNamedSilentFail("Stun");
         public float explosionSize = 2f;
         public bool explosiveKnockback = false;
         public float knockbackChance = 0.2f;
-        public SoundDef knockbackSound = SoundDefOf.Pawn_Melee_Punch_HitPawn;
+        public SoundDef knockbackSound = DefDatabase<SoundDef>.GetNamedSilentFail("Pawn_Melee_Punch_HitPawn");
         public IntRange knockDistance = new IntRange(2, 3);
         public float stunChance = -1f;
         public int stunTicks = 60;
