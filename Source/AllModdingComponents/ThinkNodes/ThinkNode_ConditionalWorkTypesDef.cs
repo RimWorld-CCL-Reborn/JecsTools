3cf20cd0b73ae14d6927a8e1e09420c61a2d8730
Alice Cecil <alycecil@gmail.com>
2018-10-31 22:32:48 -0400

    Create ThinkNodes Project

---
+++ Source/AllModdingComponents/ThinkNodes/ThinkNode_ConditionalWorkTypesDef.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace ThinkNodes
+{
+    public class ThinkNodeConditionalWorkTypes : ThinkNode_Conditional
+    {
+        private List<WorkTypeDef> workTypeDefs = new List<WorkTypeDef>();
+
+        protected override bool Satisfied(Pawn pawn)
+        {
+            foreach (var _def in workTypeDefs)
+            {
+                if (pawn.IsColonist && (pawn?.workSettings?.WorkIsActive(_def) ?? false))
+                    return true;
+            }
+
+
+            return false;
+        }
+    }
+}
