6fa441b451d7d6be2959cbf75a38d09f3a40d31f
Alice Cecil <alycecil@gmail.com>
2018-10-31 22:35:32 -0400

    Lift from my guard duty mod : JobGiver_GoToClosestThingDef
    ```
    <li Class="ThinkNode.JobGiver_GoToClosestThingDef><danger>Some</danger><thingDef>PartySpot</thingDef></li>
    ```

---
+++ Source/AllModdingComponents/ThinkNodes/JobGiver_GoToClosestThingDef.cs
@@ -0,0 +1,66 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace ThinkNodes
+{
+    public class JobGiver_GoToClosestThingDef : ThinkNode_JobGiver
+    {
+        Danger maxDanger = Verse.Danger.Some;
+        ThingDef thingDef = ThingDefOf.PartySpot;
+
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            var thing = ThingToDo(pawn);
+            
+            if (thing != null)
+            {
+                if (!thing.Position.IsValid)
+                    return (Job) null;
+                if (!pawn.CanReach((LocalTargetInfo) thing.Position, PathEndMode.ClosestTouch, maxDanger))
+                {
+                    return (Job) null;
+                }
+
+                if (IntVec3Utility.DistanceTo(thing.Position, pawn.Position) < 10f)
+                {
+                    return null;
+                }
+                
+                return new Job(JobDefOf.Goto, (LocalTargetInfo) thing.Position)
+                {
+                    locomotionUrgency = LocomotionUrgency.Sprint
+                };
+            }
+            else
+                return null;
+        }
+
+        private Thing ThingToDo(Pawn pawn)
+        {
+            ThingDef singleDef = WhatDef();
+
+            var thingRequest = ThingRequest.ForDef(singleDef);
+            Thing closestPosition = ClosestPosition(pawn, thingRequest);
+            return closestPosition;
+        }
+
+        private Thing ClosestPosition(Pawn pawn, ThingRequest thingRequest)
+        {
+            return GenClosest.ClosestThingReachable(pawn.Position, pawn.Map,
+                thingRequest, PathEndMode.Touch,
+                Danger(pawn),
+                200f);
+        }
+
+        private TraverseParms Danger(Pawn pawn)
+        {
+            return TraverseParms.For(pawn, maxDanger);
+        }
+
+        protected virtual ThingDef WhatDef()
+        {
+            return thingDef;
+        }
+    }
+}
