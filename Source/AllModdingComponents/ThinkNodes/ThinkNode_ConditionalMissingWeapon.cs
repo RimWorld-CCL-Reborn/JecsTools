65999885365d430e8ec7eef20adb5c44da5daed0
Alice Cecil <alycecil@gmail.com>
2018-11-01 20:05:39 -0400

    Extract ThinkNodes from my weapon outfits mod.
    `ThinkNodes.ThinkNode_ConditionalHunter`
    - allowBrawlers - true,false
    `ThinkNode_ConditionalDelay`
    - min, max - Whole Numbers
    `ThinkNode_ConditionalMissingHuntingWeapon`
    - No properties
    
    Example Patch:
    
    ```
    <?xml version="1.0" encoding="UTF-8"?>
    <Patch>
        <Operation Class="PatchOperationInsert">
            <xpath>/Defs/ThinkTreeDef[defName = "Humanlike"]/thinkRoot/subNodes/li[@Class="ThinkNode_SubtreesByTag"]</xpath>
            <value>
                <li Class="ThinkNode_ConditionalDrafted">
                    <invert>true</invert>
                    <subNodes>
                        <li Class="ThinkNodes.ThinkNode_ConditionalDelay">
                            <min>1000</min>
                            <max>5000</max>
                            <subNodes>
                              ...
                            </subNodes>
                        </li>
                        <li Class="ThinkNodes.ThinkNode_ConditionalHunter">
                            <allowBrawlers>false</allowBrawlers>
                            <subNodes>
                                <li Class="ThinkNodes.ThinkNode_ConditionalMissingHuntingWeapon">
                                    <subNodes>
                                       ...
                                    </subNodes>
                                </li>
                            </subNodes>
                        </li>
                    </subNodes>
                </li>
            </value>
        </Operation>
    </Patch>
    ```

---
+++ Source/AllModdingComponents/ThinkNodes/ThinkNode_ConditionalMissingWeapon.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace ThinkNodes
+{
+    public class ThinkNode_ConditionalMissingHuntingWeapon : ThinkNode_Conditional
+    {
+        protected override bool Satisfied(Pawn pawn)
+        {
+            return !WorkGiver_HunterHunt.HasHuntingWeapon(pawn);
+        }
+    }
+}
